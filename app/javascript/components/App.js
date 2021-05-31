c0df4a1e349eb3e8064c250a21dde30444d5a220
OM Miles <miles@ommiles.com>
2021-05-31 10:20:43 -0700

    impliment route in App.js

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import Editor from './Editor';
 
 const App = () => (
   <div>
+    <Route path="/events/:id?" component={Editor} />
     <Editor />
   </div>
 );
