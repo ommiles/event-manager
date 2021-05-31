8bee0626eee43bb8850cb7d6130ec32ef499f33e
OM Miles <miles@ommiles.com>
2021-05-30 22:21:35 -0700

    add BrowserRouter to application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,8 +15,16 @@
 
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from '../components/App';
 
+// wrap the app in a <BrowserRouter> component,
+// this uses the HTML5 history API to keep the UI in sync with the URL
 document.addEventListener('DOMContentLoaded', () => {
-  render(<App />, document.querySelector('#root'));
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    document.querySelector('#root'),
+  );
 });
