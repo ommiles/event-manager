070c2727393fac9f0b76758ff5a68286cc74a9d2
OM Miles <miles@ommiles.com>
2021-05-30 22:17:32 -0700

    organize react router routes in config.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,13 @@ Rails.application.routes.draw do
   # site controller is the root of our new project
   root 'site#index'
 
+  # organize react router routes
+  # we are informing Rails about the routes we will be using in our React application
+  get 'events', to: 'site#index'
+  get 'events/new', to: 'site#index'
+  get 'events/:id', to: 'site#index'
+  get 'events/:id/edit', to: 'site#index'
+
   # The routing for the controller has to consider the fact that it’s within the Api namespace.
   # We use the namespace method to fix up the routes in config/routes.rb.
   namespace :api do
