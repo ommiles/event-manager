82842c471b7d3fadc24e42b780e1caf2b1f4f063
OM Miles <miles@ommiles.com>
2021-05-30 19:08:22 -0700

    fix up routes in config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # The routing for the controller has to consider the fact that it’s within the Api namespace.
+  # We use the namespace method to fix up the routes in config/routes.rb.
+  namespace :api do
+    resources :events, only: %i[index show create destroy update]
+  end
 end
