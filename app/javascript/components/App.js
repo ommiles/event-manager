762d80542105f769f8fdbec3c287ffa70f473a90
OM Miles <miles@ommiles.com>
2021-05-31 10:22:36 -0700

    notes on our route

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import Editor from './Editor';
 
+// we have made the :id part of the route optional (due to the question mark)
+// we are pointing our root route at /events
+// By doing things this way, we will have access to the URL params within the <Editor> component.
+
 const App = () => (
   <div>
     <Route path="/events/:id?" component={Editor} />
