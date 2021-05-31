f49afb88806158b5099e6b6df6532e7d33f86038
OM Miles <miles@ommiles.com>
2021-05-30 21:05:23 -0700

    update webpacker's js pack tag and application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,14 +3,19 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+// import Rails from "@rails/ujs"
+// import Turbolinks from "turbolinks"
+// import * as ActiveStorage from "@rails/activestorage"
+// import "channels"
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+// Rails.start()
+// Turbolinks.start()
+// ActiveStorage.start()
 
-const {a, b, c, ...rest} = {a: 1, b: 2, c: 3, d: 4, e: 5};
-console.log(rest);
+import React from 'react';
+import { render } from 'react-dom';
+import App from '../components/App';
+
+document.addEventListener('DOMContentLoaded', () => {
+    render(<App />, document.querySelector('#root'));
+});
