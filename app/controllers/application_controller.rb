8ade3923d59970984c4a57a6f9eba210b8a15b3e
OM Miles <miles@ommiles.com>
2021-05-30 19:05:32 -0700

    more info on Rails CSRF protection for SPA

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
+# Rails has a built in mechanism to protect against cross site request forgery (CSRF) attacks. 
+# By default this sees Rails generate a unique token and validate its authenticity with each POST PUT PATCH DELETE request. 
+# If the token is missing, Rails will throw an exception.
+# However, as we are building a single-page app, we will only have a fresh token upon first render, which means we will need to alter this behaviour. 
+# The code below ensures that if no CSRF token is provided, Rails will respond with an empty session, which is fine for our purposes.
 class ApplicationController < ActionController::Base
-    
+
     protect_from_forgery with: :null_session
 end
